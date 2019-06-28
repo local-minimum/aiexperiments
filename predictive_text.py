bbf1eabfd1aeffa2ad1637e5200f4ef83b7feb85
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-06-28 19:30:51 +0200

    Worked a bit on predictive texts

--- predictive_text.py
+++ predictive_text.py
@@ -4,7 +4,7 @@ import re
 
 from keras.layers import Dense, Dropout, Embedding, LSTM, TimeDistributed, Activation
 from keras.models import Sequential, load_model
-from keras.preprocessing import sequence
+# from keras.preprocessing import sequence
 from keras.utils.np_utils import to_categorical
 
 import numpy as np
@@ -22,7 +22,7 @@ def read_text(path, chapter_byminlength=10, trim=True, chapter='---', extra_path
     def generator():
         for line in content:
             line = line.strip()
-            if chapter_byminlength is not None and len(line) < chapter_byminlength:                
+            if chapter_byminlength is not None and len(line) < chapter_byminlength:
                 yield
             elif line == chapter:
                 yield
@@ -80,10 +80,13 @@ def to_chapters(generator, dictionary):
         yield phrase
 
 
-def to_train_x_y(input_generator, sequence_length=5):
+def to_train_x_y(input_generator, sequence_length=5, predict_words=1):
     for line in input_generator:
-        for idx in range(len(line) - sequence_length):
-            yield line[idx: idx + sequence_length], line[idx + 1: idx + 1 + sequence_length]
+        for idx in range(len(line) - (sequence_length + predict_words - 1)):
+            yield (
+                line[idx: idx + sequence_length],
+                line[idx + predict_words: idx + predict_words + sequence_length]
+            )
 
 
 
@@ -100,12 +103,18 @@ def get_model(num_words, nodes=(50, 100, 250), sequence_length=5):
 
 
 class PredictiveText:
-    def __init__(self, model, dictionary, reverse_dictionary, training_data):
+    def __init__(
+        self, model, dictionary, reverse_dictionary, training_data,
+        training_data_y=None, batch_size=None, predict_words=1,
+    ):
         self._training_data = training_data
+        self._training_data_y = training_data_y
+        self.batch_size = batch_size
         self.model = model
         self.dictionary = dictionary
         self.reverse_dictionary = reverse_dictionary
         self.sentence_punctuation = '!.?'
+        self.predict_words = predict_words
 
     def to_text(self, sequence):
         phrase = ' '.join([self.reverse_dictionary[word] for word in sequence])
@@ -128,7 +137,24 @@ class PredictiveText:
         sequenceidx = np.random.randint(len(starts))
         return self._training_data[sequenceidx]
 
-    def speak(self, seed=None, maxlen=300, max_sentences=5):
+    def train(self, epochs=10, eons=42, speaks_each_eon=3):
+
+        if self._training_data_y is None:
+            print("Sorry no y data, can't train further")
+            return
+
+        for _ in range(eons):
+            self.model.fit(
+                np.array(self._training_data),
+                self._training_data_y,
+                batch_size=self.batch_size,
+                epochs=epochs,
+            )
+            for __ in range(speaks_each_eon):
+                print("---")
+                print(self.speak(careful=True))
+
+    def speak(self, seed=None, maxlen=300, max_sentences=5, careful=False):
         """Produce text.
 
         Arguments:
@@ -140,21 +166,34 @@ class PredictiveText:
                 How many words the output may be
             max_sentences
                 How many sentences the output may be
+            careful
+                If True will use the part of the seed that doesn't
+                overlap for next iteration's seed
 
         Returns:
             A generated text
         """
         if seed is None:
             seed = self.get_random_seed()
+        sequence_length = len(seed)
         phrase = [w for w in seed]
         nextword = seed[-1]
         sentences = 0
+        assert len(seed[-self.predict_words:]) == self.predict_words
         while sentences < max_sentences and len(phrase) < maxlen:
-            nextword = np.argmax(self.model.predict(np.array(seed)[np.newaxis, ...])[0], axis=1)[-1]
-            phrase.append(nextword)
-            seed = seed[1:] + [nextword]
-            if self.reverse_dictionary[nextword] in '!.?':
-                sentences += 1
+            nextsequence = np.argmax(
+                self.model.predict(np.array(seed)[np.newaxis, ...])[0],
+                axis=1,
+            ).tolist()
+            phrase.extend(nextsequence[-self.predict_words:])
+            for word in nextsequence[-self.predict_words:]:
+                if self.reverse_dictionary[word] in '!.?':
+                    sentences += 1
+            if careful:
+                seed = seed + nextsequence
+                seed = seed[-sequence_length:]
+            else:
+                seed = nextsequence
         return self.to_text(phrase)
 
     def save(self, path):
@@ -162,8 +201,11 @@ class PredictiveText:
         with open(path + '.params', 'wb') as f:
             pickle.dump({
                 'training_data': self._training_data,
+                'training_data_y': self._training_data_y,
+                'batch_size': self.batch_size,
                 'dictionary': self.dictionary,
                 'reverse_dictionary': self.reverse_dictionary,
+                'predict_words': self.predict_words,
             }, f)
 
     @staticmethod
@@ -176,7 +218,7 @@ class PredictiveText:
 
 def train(
     path, *extra_paths, chapter_byminlength=10, model=None,
-    batch_size=64, epochs=30, sequence_length=5
+    batch_size=64, epochs=30, sequence_length=5, predict_words=1,
 ):
     """Train based on text.
 
@@ -200,13 +242,19 @@ def train(
         sequence_length:
             How many words are given to guess what the
             next word is.
+        predict_words:
+            Number of words to predict, max is the sequence
+            length
 
     Returns:
         A predictive text result (`PredictiveText`)
 
     """
-    content = read_text(path, chapter_byminlength=chapter_byminlength, extra_paths=extra_paths)
-    dictionary, reverse_dictionary = get_dictionaries(content)  
+    predict_words = min(sequence_length, predict_words)
+    content = read_text(
+        path, chapter_byminlength=chapter_byminlength, extra_paths=extra_paths,
+    )
+    dictionary, reverse_dictionary = get_dictionaries(content)
     nwords = len(dictionary)
     print("{} different words".format(nwords))
     chapters = list(to_chapters(content, dictionary))
@@ -215,10 +263,17 @@ def train(
         min(len(chapter) for chapter in chapters),
         max(len(chapter) for chapter in chapters),
     ))
-    x, y = zip(*to_train_x_y(chapters, sequence_length))
+    x, y = zip(*to_train_x_y(
+        chapters, sequence_length, predict_words=predict_words,
+    ))
     y = to_categorical(np.array(y), nwords)
     if model is None:
         model = get_model(nwords, sequence_length=sequence_length)
-    model.fit(np.array(x), y, batch_size=batch_size, epochs=epochs)
-    pred_text = PredictiveText(model, dictionary, reverse_dictionary, x)
-    return pred_text 
+    else:
+        print("Continuing training previous model")
+    pred_text = PredictiveText(
+        model, dictionary, reverse_dictionary, x, y, batch_size,
+        predict_words=predict_words,
+    )
+    pred_text.train(epochs=epochs, eons=1)
+    return pred_text
